b2fd51b1b065c503623639bf9d560fd9125010cb
monkeytrader94 <theawesomeallan@gmail.com>
2021-12-22 11:04:03 +0800

    string_manipulation

---
+++ find_a_string.py
@@ -0,0 +1,17 @@
+def count_substring(string, sub_string):
+
+    count = 0
+    sub_string_length = len(sub_string)
+    
+    for i in range(len(string) - sub_string_length + 1):
+        if string[i:i+sub_string_length] == sub_string:
+            count += 1
+            
+    return count
+    
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
